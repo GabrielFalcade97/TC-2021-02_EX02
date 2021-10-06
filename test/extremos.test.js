9f02a76a2ae02c4cae0f2148b98d3b7d36968fee
Gabriel Falcade <80369496+GabrielFalcade97@users.noreply.github.com>
2021-10-05 21:08:37 -0300

    testing arrays

---
+++ test/extremos.test.js
@@ -0,0 +1,25 @@
+const Extremos = require("../lib/extremos");
+
+const extremos = new Extremos();
+
+describe("Extremos", () => {
+    test("testing a small array", () => {
+      expect(extremos.acharExtremos([6, 7, 8, 9, 10, 11])).toMatchObject({
+
+        menor: 6,
+        menorVal: 0,
+        maior: 11,
+        maiorVal: 10,
+
+      });
+    });
+  
+    test("testing a unordened array", () => {
+      expect(extremos.acharExtremos([1, 88, 3, 7, -5])).toMatchObject({
+        menor: -5,
+        imenorValr: 3,
+        maior: 88,
+        maiorVal: 1,
+      });
+    });
+  });
